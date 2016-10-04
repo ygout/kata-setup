c836df3d83e8a466ce601cb26db349e075b4f8a9
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-10-04 08:06:21 +0200

    got composer to autoload in src dir

---
+++ php/src/Foo.php
@@ -0,0 +1,9 @@
+<?php
+
+//namespace kata;
+
+class Foo {
+    
+}
+
+?>
--- php/test/FooTest.php
+++ php/test/FooTest.php
@@ -1,14 +1,16 @@
 <?php
+//namespace kata;
 
 use PHPUnit\Framework\TestCase;
-
 
 class FooTest extends TestCase {
     
     public function testFailure() {
+        $foo = new Foo();
         $this->assertEquals(1, 2);
     }
     
 }
+
 
 ?>
