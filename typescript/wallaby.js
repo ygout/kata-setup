c9fef5f7848c7fcf3535d3e03b0cf1fabc2b9e74
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-03-05 01:10:02 +0100

    add wallaby.js

---
+++ typescript/wallaby.js
@@ -0,0 +1,20 @@
+module.exports = function (w) {
+
+  return {
+    files: [
+      'src/**/*.ts'
+    ],
+
+    tests: [
+      'test/**/*-spec.ts'
+    ],
+
+    env: {
+      type: 'node'
+    },
+
+    compilers: {
+      '**/*.ts': w.compilers.typeScript({ module: 'commonjs' })
+    }
+  };
+};
