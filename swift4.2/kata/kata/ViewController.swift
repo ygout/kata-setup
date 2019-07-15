e91e08e86a5452b41b651da66afe6d84a741ecec
johan martinsson <jm1974@hotmail.com>
2019-07-15 09:22:39 +0200

    add swift

---
+++ swift4.2/kata/kata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kata
+//
+//  Created by johan martinsson on 15/07/2019.
+//  Copyright © 2019 johan martinsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
