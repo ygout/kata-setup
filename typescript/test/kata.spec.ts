66af7645006ea6f9d53c64bfd1403dfd22a62df7
Johan Martinsson <martinsson@users.noreply.github.com>
2023-11-20 01:40:17 +0100

    update typescript and switch to jest

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -1,9 +1,8 @@
-
-import { assert, expect } from 'chai';
+import {describe, expect, it} from '@jest/globals';
 
 describe('classname here', () => {
   it('description here', () => {
-      expect(true).equal(false);
+      expect(true).toEqual(false);
   });
 
 
