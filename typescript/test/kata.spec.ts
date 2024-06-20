63519dca7a2f0624c3a53896dab012df43f5a04f
Johan Martinsson <martinsson@users.noreply.github.com>
2024-06-20 13:57:33 +0200

    wip

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -1,10 +1,11 @@
 import {describe, expect, it} from '@jest/globals';
 
-describe('classname here', () => {
-  it('description here', () => {
-      expect(true).toEqual(false);
+function fizzbuzz(number: number) {
+  return "1";
+}
+
+describe('fizzibuzz', () => {
+  it('should return 1 for 1', () => {
+    expect(fizzbuzz(1)).toEqual("1")
   });
-
-
-
 });
