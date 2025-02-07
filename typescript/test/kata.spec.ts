90748af6a1b5033239afbfb395a175f754a97eaf
Yohann Goutaret <yohann.goutaret@wizbii.com>
2025-02-07 15:43:43 +0100

    bonjour mon ami

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -3,9 +3,16 @@ describe('greet function', () => {
   it('should return Bonjour, Emilie', () => {
     expect(greet('Emilie')).toEqual('Bonjour Emilie');
   });
+
+  it('should Bonjour, mon ami', () => {
+    expect(greet(null)).toEqual('Bonjour, mon ami');
+  });
 });
 
 
-function greet(name: string): string {
+function greet(name: string | null): string {
+  if(name === null) {
+    return 'Bonjour, mon ami';
+  }
   return `Bonjour ${name}`;
 }
