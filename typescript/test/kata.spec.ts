6d0ddce9b71fe283ccce63e21ae56d2a657d1076
Yohann Goutaret <yohann.goutaret@wizbii.com>
2025-02-07 15:45:00 +0100

    FRED

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -4,13 +4,20 @@ describe('greet function', () => {
     expect(greet('Emilie')).toEqual('Bonjour Emilie');
   });
 
-  it('should Bonjour, mon ami', () => {
+  it('should Bonjour, mon ami when name is null', () => {
     expect(greet(null)).toEqual('Bonjour, mon ami');
   });
+
+  it('shoud return BONJOUR FRED! when name is FRED', () => {
+    expect(greet('FRED')).toEqual('BONJOUR FRED!');
+  })
 });
 
 
 function greet(name: string | null): string {
+  if(name === 'FRED') {
+    return 'BONJOUR FRED!';
+  }
   if(name === null) {
     return 'Bonjour, mon ami';
   }
