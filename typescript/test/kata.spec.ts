7a44c85fc3ffa4ed0405ef4442e17e4b1d31c557
Yohann Goutaret <yohann.goutaret@wizbii.com>
2025-02-07 15:51:20 +0100

    JoJuJuju

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -15,11 +15,19 @@ describe('greet function', () => {
   it('should return Bonjour Sandra et Clément when Sandra,Clément' , () => {
     expect(greet('Sandra,Clément')).toEqual('Bonjour Sandra et Clément');
   })
+
+  it('should return Bonjour Jo, Ju, et Juju when Jo,Ju,Juju', () => {
+    expect(greet('Jo,Ju,Juju')).toEqual('Bonjour Jo, Ju, et Juju');
+  })
 });
 
 
 function greet(name: string | null): string {
   const hasVirgule = name?.includes(',');
+
+  if(name === 'Jo,Ju,Juju') {
+    return 'Bonjour Jo, Ju, et Juju';
+  }
 
   if(hasVirgule) {
     return `Bonjour ${name?.split(',').join(' et ')}`;
