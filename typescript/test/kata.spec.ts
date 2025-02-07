e8579d032a205722ecaab674f5b9fde88b62cb5b
Yohann Goutaret <yohann.goutaret@wizbii.com>
2025-02-07 15:37:07 +0100

    2é

--- typescript/test/kata.spec.ts
+++ typescript/test/kata.spec.ts
@@ -1,5 +1,11 @@
-import {describe, it} from '@jest/globals';
-describe('', () => {
-  it('', () => {
+import {describe, expect, it} from '@jest/globals';
+describe('greet function', () => {
+  it('should return bonjour emilie', () => {
+    expect(greet('emilie')).toEqual('bonjour emilie');
   });
 });
+
+
+function greet(name: string): string {
+  return `bonjour ${name}`;
+}
